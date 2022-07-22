229cec465db8d27450f4ca4590077a7e1c10656c
honeyismoney <honeyismoney@protonmail.com>
2022-07-22 14:25:17 +0400

    setup for learning

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,32 +1,14 @@
 import os
-from brownie import LoggerNFT, accounts, Contract
+from brownie import Graffity, accounts
 import pandas as pd
 from web3 import Web3
-from dotenv import load_dotenv
-import json
 
-
-load_dotenv()
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 
-def get_device_wallet():
-    return accounts.add(os.getenv("PRIVATE_KEY"))
-
-
-def get_logger_contract():
-    abi = json.load(open("build/contracts/LoggerNFT.json", "r"))["abi"]
-    return Contract.from_abi("LoggerNFT", os.getenv("LOGGER_ADDRESS"), abi)
-
-
-def last_timestamp(device_id):
-    loggerNFT = get_logger_contract()
-    return loggerNFT.timestamp(device_id)
-
-
-def get_logger_history(loggerNFT, id):
-    contract = w3.eth.contract(address=loggerNFT.address, abi=loggerNFT.abi)
+def get_logger_history(graffity, id):
+    contract = w3.eth.contract(address=graffity.address, abi=graffity.abi)
 
     # get the events from the contract
     filter = contract.events.LogData.createFilter(
@@ -36,5 +18,5 @@ def get_logger_history(loggerNFT, id):
 
     # create a dataframe from the events
     mappedList = map(lambda d: d.args.data, events)
-    df = pd.DataFrame(list(mappedList), columns=["temp", "gps", "timestamp"])
+    df = pd.DataFrame(list(mappedList), columns=["message", "writer"])
     return df
