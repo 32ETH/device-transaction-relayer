c13c6894513704df674a0a8a3941f5e46aabc27a
honeyismoney <honeyismoney@protonmail.com>
2022-07-22 14:47:48 +0400

    tutorial

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-
 from brownie import Graffity, accounts
+from . import helpers
 
 deployer_account = accounts[0]
-abdullah = accounts[1]
-usama = accounts[2]
+# abdullah = accounts[1]
+# usama = accounts[2]
+# raj = accounts[3]
 
 
 def main():
-    return Graffity.deploy("A Nice clean wall", {'from': accounts[0]})
+    wall = Graffity.deploy("A Nice clean wall", {"from": accounts[0]})
+    lastMessage = wall.message()
+    print(f"\nOn the wall: {lastMessage}")
+
+    # wall.setMessage("Abdullah was here!", {"from": abdullah})
+    # wall.setMessage("Usama was here!", {"from": usama})
+    # wall.setMessage("Raj was here!", {"from": raj})
+
+    # lastMessage = wall.message()
+    # print(f"\nOn the wall: {lastMessage}\n")
+
+    # history = helpers.get_logger_history(wall)
+    # print("-------------------------- Wall History -------------------------")
+    # print(history)
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -2,21 +2,22 @@ import os
 from brownie import Graffity, accounts
 import pandas as pd
 from web3 import Web3
+from pprint import pprint
+
+pp = pprint
 
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 
 
-def get_logger_history(graffity, id):
+def get_logger_history(graffity):
     contract = w3.eth.contract(address=graffity.address, abi=graffity.abi)
 
     # get the events from the contract
-    filter = contract.events.LogData.createFilter(
-        fromBlock=0, arguments={"loggerId": id}
-    )
+    filter = contract.events.MessageChanged.createFilter(fromBlock=0)
     events = filter.get_all_entries()
 
     # create a dataframe from the events
-    mappedList = map(lambda d: d.args.data, events)
-    df = pd.DataFrame(list(mappedList), columns=["message", "writer"])
+    mappedList = map(lambda d: [d.args["message"], d.args["sender"]], events)
+    df = pd.DataFrame(list(mappedList), columns=["MESSAGE", "WRITER"])
     return df
